cc5aacb2ae64ab3975be1b96b27f52d6698e8f8e
divyeshbhuva1001 <divyeshbhuva1001@gmail.com>
2024-07-11 19:26:39 +0530

    landing page design issue resolve

--- src/components/homePage/HomeFooter.tsx
+++ src/components/homePage/HomeFooter.tsx
@@ -21,22 +21,22 @@ const HomeFooter = () => {
         {isHomePage() && (
           <div className="h-[230px] absolute bottom-0 bg-[#71B2C9] lg:block hidden"></div>
         )}
-        <div className="w-[1162px] bg-[#002A3A] z-[999] rounded-tl-[20px] rounded-tr-[20px] text-center">
+        <div className="w-[1162px] bg-[#002A3A] z-[999] sm:rounded-tl-[20px] sm:rounded-tr-[20px] rounded-0 text-center">
           <div className="lg:flex block xl:gap-x-[33px] gap-x-[15px] xl:ml-8 ml-0 xl:mt-[44px] mt-6 xl:justify-normal justify-center lg:pb-0 pb-5">
             <div className="relative lg:p-0 px-5">
               {/* <img
                 className="xl:w-[951px] w-[810px] h-[84px] lg:block hidden"
                 src="../assets/img/network-group.png"
               /> */}
-              <div className="flex lg:flex-row flex-col lg:justify-center justify-start lg:items-center items-start rounded-xl lg:bg-white bg-transparent xl:gap-3 gap-2 xl:w-[951px] lg:w-[850px] w-auto lg:h-[84px] h-auto lg:p-0 lg:px-5 px-0">
+              <div className="flex lg:flex-row flex-col lg:justify-center justify-start lg:items-center items-start rounded-xl lg:bg-white bg-transparent xl:gap-3 sm:gap-2 gap-0 xl:w-[951px] lg:w-[850px] w-auto lg:h-[84px] h-auto lg:p-0 lg:px-5 px-0">
                 <div className="lg:order-1 order-4">
-                  <p className="lg:w-[300px] w-full leading-4 text-justify text-sm lg:text-[#002A3A] text-white text-semibold line-clamp-4">
+                  <p className="lg:w-[300px] w-full text-justify xs:text-sm text-lg sm:leading-4 leading-5 lg:text-[#002A3A] text-white text-semibold line-clamp-4 sm:pt-0 sm:pb-0 pt-[15px] pb-[15px] sm:mb-0 mb-[52px] sm:border-b-0 border-b border-white">
                     Skillnet Ireland is funded from the National Training Fund
                     through the Department of Further and Higher Education,
                     Research, Innovation and Science.
                   </p>
                 </div>
-                <div className="lg:order-2 order-2">
+                <div className="lg:order-2 order-2 sm:pb-0 pb-[41px]">
                   <img
                     src={footerLogoImage}
                     alt="footer logo"
@@ -45,15 +45,15 @@ const HomeFooter = () => {
                   <img
                     src={footerLogoImage1}
                     alt="footer logo"
-                    className="lg:hidden block"
+                    className="lg:hidden block h-[74px] w-[150px] ml-[22px]"
                   />
                 </div>
-                <div className="secondary-text sm:flex block justify-between items-center py-3 lg:hidden lg:order-3 order-3">
+                <div className="secondary-text sm:flex block justify-between items-center lg:hidden lg:order-3 order-3">
                   <p className="lg:block hidden no-underline text-sm leading-5 font-abhaya font-bold">
                     © County Wexford Chamber 2023. All Rights Reserved.
                   </p>
 
-                  <div className="flex gap-5  justify-center my-3">
+                  <div className="flex gap-5  justify-center sm:my-3">
                     <Button
                       variant={"ghost"}
                       className="w-[19px] h-[19px] p-0 hover:bg-transparent hover:text-white"
@@ -80,7 +80,7 @@ const HomeFooter = () => {
                     </Button>
                   </div>
                 </div>
-                <div className="lg:order-3 order-1">
+                <div className="lg:order-3 order-1 sm:pb-0 pb-[34px]">
                   <img
                     src={homeFooterLogo}
                     alt=""
@@ -92,16 +92,18 @@ const HomeFooter = () => {
                     className="lg:hidden block w-[300px]"
                   />
                 </div>
-                <div className="flex items-center lg:order-4 order-4">
-                  <div>
-                    <img src={RoundLogoImage} alt="round logo" />
-                  </div>
-                  <div>
-                    <h6 className="xl:text-base text-sm font-bold lg:text-[#002A3A] text-white text-left leading-4">
-                      Co-funded by
-                      <br />
-                      the European Union
-                    </h6>
+                <div className="sm:block hidden lg:order-4 order-4">
+                  <div className="flex items-center">
+                    <div>
+                      <img src={RoundLogoImage} alt="round logo" />
+                    </div>
+                    <div>
+                      <h6 className="xl:text-base text-sm font-bold lg:text-[#002A3A] text-white text-left leading-4">
+                        Co-funded by
+                        <br />
+                        the European Union
+                      </h6>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -142,15 +144,12 @@ const HomeFooter = () => {
             </div>
 
             <div className="flex items-start">
-              <ul className=" text-white flex flex-col  gap-y-[3px] text-start secondary-text lg:p-0 px-5">
+              <ul className=" text-white flex flex-col  sm:gap-y-[3px] gap-y-[10px] text-start secondary-text lg:p-0 px-5 sm:pr-[35px] pr-[29px] underline">
                 <a className="font-abhaya text-base font-bold cursor-pointer leading-5">
                   Our Courses
                 </a>
                 <a className="font-abhaya text-base font-bold cursor-pointer leading-5">
                   Membership
-                </a>
-                <a className="font-abhaya text-base font-bold cursor-pointer leading-5">
-                  Testimonial
                 </a>
                 <a className="font-abhaya text-base font-bold cursor-pointer leading-5">
                   News
@@ -159,7 +158,7 @@ const HomeFooter = () => {
                   Contact Us
                 </a>
               </ul>
-              <p className="lg:hidden block no-underline text-sm text-white leading-5 font-abhaya font-bold min-w-[165px] text-left">
+              <p className="lg:hidden block no-underline text-sm text-white leading-5 font-abhaya font-bold max-w-[165px] w-[165px] text-left">
                 © County Wexford Chamber 2023. All Rights Reserved.
               </p>
             </div>
